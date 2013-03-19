3e24aafebb22fe81a312290113e5ed65f6ef14dc
Matt Galvin <matthewgalvin908@gmail.com>
2013-03-19 16:33:14 -0400

    rspec install, controllers, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Checkmate::Application.routes.draw do
-   root :to => 'home#index'
+  root :to => 'home#index'
+  resources :users, :only => [:new, :create, :show]
+  resources :banks, :only => [:new, :create, :show]
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
