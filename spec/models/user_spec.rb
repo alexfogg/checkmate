014838927e61beae6383dafce967eac591cd5ff5
Alex Fogg <me@alexfogg.com>
2013-03-19 16:57:48 -0400

    fixed user test bug

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,10 +6,10 @@ describe User do
     it 'creates a User' do
       user = FactoryGirl.create(:user)
       user.should be_an_instance_of(User)
-      User.name.should eq 'Bob'
-      User.email.should eq 'bob@gmail.com'
-      User.password.should eq 'a'
-      User.password_confirmation.should eq 'a'
+      user.name.should eq 'Bob'
+      user.email.should eq 'bob@gmail.com'
+      user.password.should eq 'a'
+      user.password_confirmation.should eq 'a'
     end
   end
 
