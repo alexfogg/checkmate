87c5bc940d1c988876b30c635fec6aa09ec897cc
Alex Fogg <me@alexfogg.com>
2013-03-19 19:02:48 -0400

    figuring out seeds/table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Bank.delete_all
+
+u1 = User.create(username: 'Bob', email: 'bob@gmail.com', password: 'a', password_confirmation: 'a')
+
+b1 = Bank.create(name: 'Bank of America', balance: '500')
+b2 = Bank.create(name: 'Wells Fargo', balance: '5000')
+b3 = Bank.create(name: 'Citibank', balance: '1000')
+b4 = Bank.create(name: 'PNC', balance: '800')
+b5 = Bank.create(name: 'Chase', balance: '500')
+
+u1.banks = [b1, b2, b3, b4, b5]
