d8a8e71b41373a2ec5bdf7f91e42684f8a4835b9
Matt Galvin <matthewgalvin908@gmail.com>
2013-03-19 23:43:07 -0400

    trying to add show page

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -7,5 +7,11 @@ class BanksController < ApplicationController
     @bank = Bank.order(:name)
     @banks = Bank.all
   end
+  def show
+    @bank = Bank.find(params[:id])
+  end
+  def new_transaction
+    @bank = Bank.find(params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Checkmate::Application.routes.draw do
   root :to => 'home#index'
   resources :users, :only => [:new, :create, :show]
   resources :banks, :only => [:new, :create, :show]
+  get '/show' => 'banks#new_transaction'
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
